fbb9dcf460a1147e20b52bd3265a23574dab2503
Yann PLOUGONVEN--LASTENNET <y.plougonven@workstation3.nsi.lan>
2022-09-06 15:15:41 +0200

    création d'armes

--- src/fighter_game/fighter.py
+++ src/fighter_game/fighter.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, uniform
 
 class Fighter:
     """
@@ -9,6 +9,7 @@ class Fighter:
         self._description = description
         self._agility = randint(1,9)
         self._health_points = 100 # Lors de la création d'une instance, les points de vie valent 100.
+        self._weapon = None
         
     def get_name(self):
         """Return fighter's name"""
@@ -23,18 +24,90 @@ class Fighter:
     def get_agility(self):
         return self._agility
     
-    def get_strenght(self):
+    def get_strength(self):
         return 10 - self.get_agility()
     
     def get_health_points(self):
         return self._health_points
     
+    def get_weapon(self):
+        return self._weapon
+    
+    def take_weapon(self, a_weapon): # à son initiative, le fighter prend ou change d'arme
+        if self.get_weapon(): # Si self a déjà une arme, on enlève sa pocession à son ex owner
+            old_weapon = self.get_weapon()
+            old_weapon._owner = None
+        # Dans tous les cas, on associe la nouvelle arme à son owner :
+        self._weapon = a_weapon
+        a_weapon._owner = self
+            
+    
+    def punch(self, aFighter):
+        """
+        punch aFighter
+        return the health points of AFighter
+        """
+        points=int(uniform(0.7,1.0)*10*self.get_strength()/aFighter.get_agility())
+        aFighter._health_points=aFighter.get_health_points()-points
+        print(aFighter._health_points)
+        return aFighter.get_health_points()
+    
+    def shoot(self, aFighter):
+        if self.get_weapon():
+            self.get_weapon().shoot(aFighter)
+        else :
+            self.punch(aFighter)
+        
+    
+    def summary(self):
+        name = f"name : {self.get_name()}"
+        description = f"description : {self.get_description()}"
+        agility = f"agility : {self.get_agility()}"
+        strength = f"strength : {self.get_strength()}"
+        health_points = f"health points : {self.get_health_points()}"
+        if self.get_weapon():
+            weapon = f"weapon : {self.get_weapon().get_name()}"
+        else:
+            weapon = f"weapon : "
+        return "\n".join([name, description, agility, strength, health_points, weapon])
+        
+    
+from fighter_game.weapon import Weapon
 
+lance_patates = Weapon("Lance patates", 5, 10)
+bazooka = Weapon("Bazooka", 20, 2)
+print(bazooka.summary())
 marcel = Fighter('Marcel', 'The best one') # on instancie avec les variables de la méthode __init__
 maurice = Fighter('Maurice', 'The second best one')# on instancie avec les variables de la méthode __init__
 
-print(marcel.get_agility())
-print(marcel.get_strenght())
+marcel.take_weapon(lance_patates)
+print(maurice.summary())
+marcel.shoot(maurice)
+print(maurice.summary())
+marcel.take_weapon(bazooka)
+print(bazooka.summary())
+marcel.shoot(maurice)
+print(maurice.summary())
+print(lance_patates.get_owner())
+print(bazooka.get_owner())
+print(f"\n {marcel.summary()}")
+
+# 
+# while marcel.get_health_points() > 0 and maurice.get_health_points() > 0:
+#     marcel.punch(maurice)
+#     if maurice.get_health_points() > 0:
+#         maurice.punch(marcel)
+# 
+# if marcel.get_health_points() > 0:
+#     print(f"Le gagnant est {marcel.get_name()}")
+# else :
+#     print(f"Le gagnant est {maurice.get_name()}")
+# 
+# 
+# print(marcel.summary())
+# print(maurice.summary())
+# #print(marcel.get_agility())
+# #print(marcel.get_strenght())
 
 
 
--- src/fighter_game/weapon.py
+++ src/fighter_game/weapon.py
@@ -2,4 +2,48 @@ class Weapon:
     """
     Class for a weapon
     """
-    pass
+    def __init__(self, name, damage, ammos):
+        self._name = name
+        self._damage = damage
+        self._ammos = ammos
+        self._owner = None
+    
+    def get_name(self):
+        """Return fighter's name"""
+        return self._name
+    
+    def get_damage(self):
+        return self._damage
+
+    def get_ammos(self):
+        return self._ammos
+    
+    def get_owner(self):
+        return self._owner
+    
+    
+    def shoot(self, fighter):
+        if self.get_ammos() > 0:
+            fighter._health_points -= self.get_damage()
+            self._ammos -= 1
+            
+    def summary(self):
+        name = f"name : {self.get_name()}"
+        damage = f"damage : {self.get_damage()}"
+        ammos = f"ammos : {self.get_ammos()}"
+        if self.get_owner():
+            owner = f"owner : {self.get_owner().get_name()}"
+        else:
+            owner = f"owner : "
+        return '\n'.join([name, damage, ammos, owner])    
+
+
+
+#from fighter_game.fighter import Fighter
+#marcel = Fighter('Marcel', 'The best one') # on instancie avec les variables de la méthode __init__
+#lance_patates = Weapon("Lance patates", 5, 10)
+
+#lance_patates.shoot(marcel)
+
+#print(marcel.summary())
+#print(lance_patates.get_ammos())
