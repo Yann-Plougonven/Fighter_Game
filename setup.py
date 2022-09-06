5b977eb03dcfefd0b3d8bc444de84907c5d24a8c
Yann PLOUGONVEN--LASTENNET <y.plougonven@workstation3.nsi.lan>
2022-09-06 13:31:03 +0200

    modif du setup

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
    name='FighterGame',
-   version='0.1.0',
-   author='NSI Team',
-   author_email='yves.cadour@saint-louis29.net',
+   version='0.0.1',
+   author='Yann Plougonven',
+   author_email='plougonven-y@saint-louis29.net',
    license='LICENSE.txt',
    description='An awesome fighter game',
    long_description=open('README.md').read(),
