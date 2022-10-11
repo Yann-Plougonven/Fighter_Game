3587fa8ccf76d218d36d93a1735cb758e63f4269
Yann Plougonven <plougonven-y@saint-louis29.net>
2022-10-11 15:04:48 +0200

    jeu fonctionnel

--- src/fighter_game/tournament.py
+++ src/fighter_game/tournament.py
@@ -1,3 +1,7 @@
+from fighter import Fighter
+from myqueue import Queue
+fighters_names = ['Michel', 'Jean', 'Gérard', 'Jean-Pierre', 'Chantal', 'Germaine', 'Rachel', 'Odette']
+
 
 def fight(attaquer, victim):
     """
@@ -5,10 +9,23 @@ def fight(attaquer, victim):
     Lance un combat entre 2 joueurs
     Retourne le joueur gagnant
     """
-    
-    
+    attaquer.punch(victim)
+    if victim.get_health_points() <= 0:  # Si la victime est morte
+        return attaquer
+    else :  # Si la victime est vivante
+        return fight(victim, attaquer)
+        
 def tournament(list_of_fighters):
     """
     Créer un tournoi
     Retourne le survivant du combat
     """
+    queue = Queue()
+    [queue.enqueue(fighter) for fighter in list_of_fighters]
+    while queue.size() > 1:
+        queue.enqueue(fight(queue.dequeue(), queue.dequeue()))    
+    return queue.dequeue()
+
+
+fighters = [Fighter(name, "combatant") for name in fighters_names]
+print(tournament(fighters).get_name())
