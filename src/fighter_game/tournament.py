5f6efe846fe218b0ae14731aff4e2870d778334a
Yann Plougonven <plougonven-y@saint-louis29.net>
2022-10-11 14:12:58 +0200

    Préparation

---
+++ src/fighter_game/tournament.py
@@ -0,0 +1,14 @@
+
+def fight(attaquer, victim):
+    """
+    En récursif
+    Lance un combat entre 2 joueurs
+    Retourne le joueur gagnant
+    """
+    
+    
+def tournament(list_of_fighters):
+    """
+    Créer un tournoi
+    Retourne le survivant du combat
+    """
